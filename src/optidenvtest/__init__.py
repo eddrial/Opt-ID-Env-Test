09d5914c8720531e4b80c323e3a144ad9d8f81c5
JossWhittle <joss.whittle@gmail.com>
2020-10-28 16:42:37 +0000

    Added dummy src file to enable code coverage tests

--- src/optidenvtest/__init__.py
+++ src/optidenvtest/__init__.py
@@ -11,3 +11,6 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 # either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
+
+print('Dummy src file to test code coverage!')
